4d6e820d320537f9f1d08cfb2ca8932c71887171
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2014-09-09 10:58:13 -0500

    Se quita la pendejada recursiva y se resulve el laberinto

--- intento_1/com/rod/Main.groovy
+++ intento_1/com/rod/Main.groovy
@@ -5,30 +5,38 @@ def laberinto = {
 }()
 
 def bloques = []
-def bloque = { lab ->	
-	lab.eachLine { linea, index ->
-		def yCont = 0
-		linea.getChars().each { b ->
-			bloques << new Bloque(
-				x: index, y: yCont++, tipo: b
-			)
-		}
-	}
-	return { x, y -> 
-		bloques.find { 
-				it.x == x && it.y == y
-		}		
-	}
+def bloque = { lab -> 
+  lab.eachLine { linea, index ->
+    def yCont = 0
+    linea.getChars().each { b ->
+      bloques << new Bloque(
+        x: index, y: yCont++, tipo: b
+      )
+    }
+  }
+  return { x, y -> 
+    bloques.find { 
+        it.x == x && it.y == y
+    }   
+  }
 }(laberinto)
 
 def setVecinos = {
-	bloques.collect { b ->
-		b.abajo = bloque(b?.x + 1, b?.y)
-		b.derecha = bloque(b?.x, b?.y + 1)
-		b.izquierda = bloque(b?.x, b?.y - 1)
-		b.arriba = bloque(b?.x - 1, b?.y) 
-	}
+  bloques.collect { b ->
+    b.abajo = bloque(b?.x + 1, b?.y)
+    b.derecha = bloque(b?.x, b?.y + 1)
+    b.izquierda = bloque(b?.x, b?.y - 1)
+    b.arriba = bloque(b?.x - 1, b?.y) 
+  }
 }()
+
+//Tipo de bloques
+Character bLibre = ' '
+Character bOcupado = '*'
+Character bActual = 'A'
+Character bVisitado = '1'
+Character bRepetido = '2'
+Character bCamino = '+'
 
 def pintaLaberinto = {
   def lCount = 0
@@ -37,54 +45,67 @@ def pintaLaberinto = {
       lCount++
       print '\n'
     }
-    print b.tipo    
+    //print b.tipo
+    
+    if(b.tipo in [bActual, bVisitado]) {
+      print bCamino      
+    } 
+    else if(b.tipo == bRepetido) {
+      print bLibre
+    } else {
+      print b.tipo
+    }
   }  
   print '\n'
 }
-
-//Tipo de bloques
-Character bLibre = ' '
-Character bOcupado = '*'
-Character bVisitado = '1'
-Character bRepetido = '2'
 
 def entrada = bloques.find { 
     it.x == 0 && it.tipo == bLibre 
 }
 
-def recorrer
-recorrer = { b ->
-  if(b != null) {        
-    if(b.tipo == bLibre) { // Marca visitados
-      b.tipo = bVisitado
-    }
-    if(b.abajo?.tipo == bLibre) { // Abren camino
-      recorrer(b?.abajo)
-    } else if(b.derecha?.tipo == bLibre) {
-      recorrer(b?.derecha)
-    } else if(b.izquierda?.tipo == bLibre) {
-      recorrer(b?.izquierda)
-    } else if(b.arriba?.tipo == bLibre) {
-      recorrer(b?.arriba)
-    } else if(b.derecha?.tipo == bVisitado) { // Repiten camino
+entrada.tipo = bActual
+
+def asignaActual = { b ->
+  b.tipo = bActual
+  if(b.abajo && b.abajo.tipo == bLibre) { //Abren camino
+    b.tipo = bVisitado
+    b.abajo.tipo = bActual    
+  } else if(b.derecha && b.derecha.tipo == bLibre) {
+    b.tipo = bVisitado
+    b.derecha.tipo = bActual    
+  } else if(b.izquierda && b.izquierda.tipo == bLibre) {
+    b.tipo = bVisitado
+    b.izquierda.tipo = bActual    
+  } else if(b.arriba && b.arriba.tipo == bLibre) {
+    b.tipo = bVisitado
+    b.arriba.tipo = bActual    
+  } else if(b.derecha && b.derecha.tipo == bVisitado) { // Repiten camino
       b.tipo = bRepetido
-      recorrer(b?.derecha)
-    } else if(b.arriba?.tipo == bVisitado) {
+      b.derecha.tipo = bActual      
+  } else if(b.arriba && b.arriba.tipo == bVisitado) {
       b.tipo = bRepetido
-      recorrer(b?.arriba)
-    } else if(b.abajo?.tipo == bVisitado) {
+      b.arriba.tipo = bActual      
+  } else if(b.izquierda && b.izquierda.tipo == bVisitado) {
       b.tipo = bRepetido
-      recorrer(b?.abajo)
-    } else if(b.izquierda?.tipo == bVisitado) {
+      b.izquierda.tipo = bActual
+  } else if(b.abajo && b.abajo.tipo == bVisitado) {
       b.tipo = bRepetido
-      recorrer(b?.izquierda)
-    }
-  }
-  return null
+      b.abajo.tipo = bActual
+  } else {    
+    return false
+  } 
+  return true 
 }
-
-recorrer(entrada)
-
-
+def seguir = true
+while(seguir) {
+  def actual = bloques.find { 
+    it.tipo == bActual
+  }
+  if(actual.abajo == null) {
+    seguir = false
+  } else {
+    seguir = asignaActual(actual)
+  }
+}
 
 pintaLaberinto()
