74f53920a718916a2619089bfab26ea947883c1e
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2014-09-09 10:01:08 -0500

    Nesecita mucha memoria, por el pinche "recorrer"

--- intento_1/com/rod/Main.groovy
+++ intento_1/com/rod/Main.groovy
@@ -1,10 +1,9 @@
 package com.rod
 
-def laberinto = """\
-************************* *************************
-*                                   * *           *
-* * *** *** ******************* ***** * * * * * ***
-"""
+def laberinto = {
+  new File('com/rod/laberinto.txt').text
+}()
+
 def bloques = []
 def bloque = { lab ->	
 	lab.eachLine { linea, index ->
@@ -30,3 +29,62 @@ def setVecinos = {
 		b.arriba = bloque(b?.x - 1, b?.y) 
 	}
 }()
+
+def pintaLaberinto = {
+  def lCount = 0
+  bloques.each { b ->
+    if(b.x != lCount) {
+      lCount++
+      print '\n'
+    }
+    print b.tipo    
+  }  
+  print '\n'
+}
+
+//Tipo de bloques
+Character bLibre = ' '
+Character bOcupado = '*'
+Character bVisitado = '1'
+Character bRepetido = '2'
+
+def entrada = bloques.find { 
+    it.x == 0 && it.tipo == bLibre 
+}
+
+def recorrer
+recorrer = { b ->
+  if(b != null) {        
+    if(b.tipo == bLibre) { // Marca visitados
+      b.tipo = bVisitado
+    }
+    if(b.abajo?.tipo == bLibre) { // Abren camino
+      recorrer(b?.abajo)
+    } else if(b.derecha?.tipo == bLibre) {
+      recorrer(b?.derecha)
+    } else if(b.izquierda?.tipo == bLibre) {
+      recorrer(b?.izquierda)
+    } else if(b.arriba?.tipo == bLibre) {
+      recorrer(b?.arriba)
+    } else if(b.derecha?.tipo == bVisitado) { // Repiten camino
+      b.tipo = bRepetido
+      recorrer(b?.derecha)
+    } else if(b.arriba?.tipo == bVisitado) {
+      b.tipo = bRepetido
+      recorrer(b?.arriba)
+    } else if(b.abajo?.tipo == bVisitado) {
+      b.tipo = bRepetido
+      recorrer(b?.abajo)
+    } else if(b.izquierda?.tipo == bVisitado) {
+      b.tipo = bRepetido
+      recorrer(b?.izquierda)
+    }
+  }
+  return null
+}
+
+recorrer(entrada)
+
+
+
+pintaLaberinto()
